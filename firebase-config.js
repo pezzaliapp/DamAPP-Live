8ecf348e5961f90591b89e368967cde49abeee08
pezzaliapp <pezzalialessandro@gmail.com>
2025-04-12 20:43:35 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-
+// Sostituisci i valori con quelli del tuo progetto Firebase, se necessario
 const firebaseConfig = {
   apiKey: "AIzaSyDBE9MmhzLZ3h-vIRI-sJi_0TBINAHiqcI",
   authDomain: "damapp-live.firebaseapp.com",
@@ -8,4 +8,5 @@ const firebaseConfig = {
   messagingSenderId: "200514528885",
   appId: "1:200514528885:web:943d6def84eb3d12a5fa3c"
 };
+
 firebase.initializeApp(firebaseConfig);
